5386f0cb5fafa00287ca8601615407dd7184fd2e
Syed Ahmed <syedahmed@syeds-air.mynetworksettings.com>
2023-09-20 19:37:52 -0400

    fixed routes

--- server.js
+++ server.js
@@ -23,11 +23,5 @@ app.use("/bookmarks", bmarkController);
 
 
 
-=======
-const express = require('express');
-const app = express()
-
-
-
 
 app.listen(5100, () => {console.log('Server is running on port 5100')});
